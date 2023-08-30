8fcc54b03aac86dc0c28e0237dd121ec780a56d9
Aayushhhhhhhhhhhhhhhhhhhhh <143619742+Aayushhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2023-08-30 19:01:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width: 350,
+    height: 380,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach(' #camera')
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image"  src = "' + data_uri + '"  />';
+
+    });
+
+}
