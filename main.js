343fbe9f7254464be8bc1e5634db09024cf217ec
Aayushhhhhhhhhhhhhhhhhhhhh <143619742+Aayushhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2023-08-30 19:25:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 
-Webcam.attach(' #camera')
+Webcam.attach(camera)
 
 function takesnapshot() {
     Webcam.snap(function (data_uri) {
@@ -15,4 +15,29 @@ function takesnapshot() {
 
     });
 
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CaUPW_ioe/model.json', modelLoaded);
+function modelLoaded() {
+    console.log("modelLoadded")
+
+}
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotresult);
+
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+    }
 }
